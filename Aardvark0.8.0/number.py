191467d6292d16f2872f2029b25ddafeec37665d
theboys619 <theboys619@gmail.com>
2020-10-30 18:00:46 -0400

    Removed python interpreter

--- Aardvark0.8.0/number.py
+++
@@ -1,8 +0,0 @@
-from Aardvark import *
-@Aardvark.type("number")
-def number(what, line_num):
-    what=str(what)
-    for i in str(what):
-        if i not in "0123456789.+-*%^/":
-            return False, what
-    return True, eval(str(what))
